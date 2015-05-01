b30cc2f9b3c90948fa318d90290975ab7ab9acf5
Kevin Mottashed <kevinmottashed@gmail.com>
2015-04-30 22:04:01 -0400

    Added another 2 tasks. Task 1 yields, task 2 runs, tasks 3 and 4 sleep.

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 
 void task1(void * arg);
 void task2(void * arg);
+void task3(void * arg);
+void task4(void * arg);
 
 void task1(void * arg)
 {
@@ -17,7 +19,7 @@ void task1(void * arg)
   {
     for (int i = 0; i < 0x10000; ++i)
     {
-      if (i & 0xFF == 0)
+      if ((i & 0xFF) == 0)
       {
         yield();
       }
@@ -36,9 +38,27 @@ void task2(void * arg)
   }
 }
 
+void task3(void * arg)
+{
+  while (true)
+  {
+    sleep(3);
+  }
+}
+
+void task4(void * arg)
+{
+  while (true)
+  {
+    sleep(5);
+  }
+}
+
 int main()
 {
   kernel_create_task(&task1);
   kernel_create_task(&task2);
+  kernel_create_task(&task3);
+  kernel_create_task(&task4);
   kernel_main();
 }

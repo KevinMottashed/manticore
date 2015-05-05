a09d96b924bd4576d963c50ff82258b83ac72bbf
Kevin Mottashed <kevinmottashed@gmail.com>
2015-05-04 22:43:15 -0400

    Added the beer-ware license.
    I've always wanted to use this license.

--- hardware.c
+++ hardware.c
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <kevinmottashed@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. 
+ * -Kevin Mottashed
+ * ----------------------------------------------------------------------------
+ */
+
 #include "hardware.h"
 
 #include "system.h"
--- kernel.h
+++ kernel.h
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <kevinmottashed@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. 
+ * -Kevin Mottashed
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef KERNEL_H
 #define KERNEL_H
 

843b0efc0c465cc3b6afe3bfcf08d76b7ab669e2
Kevin Mottashed <kevinmottashed@gmail.com>
2015-12-15 21:04:24 -0500

    Added comment about SVC ISR
    The SysTick can't preempt SVC because they're at the same priority. I
    added a comment so I don't forget in the future.

--- syscall_isr.s
+++ syscall_isr.s
@@ -3,46 +3,46 @@
  * "THE BEER-WARE LICENSE" (Revision 42):
  * <kevinmottashed@gmail.com> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return. 
+ * this stuff is worth it, you can buy me a beer in return.
  * -Kevin Mottashed
  * ----------------------------------------------------------------------------
  */
 
   NAME syscall
-  
+
   PUBLIC SVCall_Handler
-  
+
   IMPORT SaveContext
   IMPORT RestoreKernel
   IMPORT syscallValue
   IMPORT SysTickCtrlAddr
-  
+
   SECTION .data : CONST (2)
 PendStClear
   DC32 0x02000000
-  
+
 ICSR ; Interrupt control and status register
   DC32 0xE000ED04
-  
+
   SECTION .text : CODE (2)
   THUMB
-  
+
 DisableSysTickIrq:
   ; Disable the systick ISR
   LDR R0, =SysTickCtrlAddr
   LDR R0, [R0]
   MOVS R1, #1
   STR R1, [R0]
-  
+
   ; Clear a pending systick interrupt
   LDR R0, =ICSR
   LDR R0, [R0]
   LDR R1, =PendStClear
   LDR R1, [R1]
   STR R1, [R0]
-  
+
   BX LR
-  
+
 ; Saves the value associated with the SVC instruction.
 ; void SaveSVC(void)
 SaveSVC:
@@ -56,13 +56,15 @@ SaveSVC:
   LDR R1, =syscallValue
   STRB R0, [R1]
   BX LR
-  
+
 SVCall_Handler:
   ; We need to save the task context, save the SVC value and then
   ; let the kernel handle the system call.
+  ; The SysTick IRQ has the same priority as SVC so we don't
+  ; need to worry about being preempted and losing the context.
   BL DisableSysTickIrq
   BL SaveContext
   BL SaveSVC
   B RestoreKernel
-  
+
   END
